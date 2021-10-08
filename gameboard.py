f4c6ee856a69336be6bfafd44b70d429478ccbbc
EricPrieto <ep1043@hotmail.com>
2021-10-08 14:15:09 -0500

    Method outlines for Gameboard class created

---
+++ gameboard.py
@@ -0,0 +1,31 @@
+from human import Human
+from ai import Ai
+
+class Gameboard:
+    def __init__(self):
+        self.human = Human()
+        self.ai = Ai()
+
+    def run_game(self):
+        pass
+
+    def display_rules(self):
+        pass
+
+    def display_welcome(self):
+        pass    
+
+    def human_pick(self):
+        pass
+
+    def ai_pick(self):
+        pass
+
+    def game(self):
+        pass
+
+    def display_gestures(self):
+        pass
+
+    def declare_winner(self):
+        pass
