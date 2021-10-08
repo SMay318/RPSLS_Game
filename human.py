ade776d60b605d36ff7d238d4292570aab361f5d
EricPrieto <ep1043@hotmail.com>
2021-10-08 14:00:19 -0500

    Child classes created

---
+++ human.py
@@ -0,0 +1,10 @@
+from player import Player
+
+class Human(Player):
+    def __init__(self, name):
+        self.name = name
+    
+        super().__init__()
+
+    def __str__(self) -> str:
+        return f"Player1 - {self.name}"
